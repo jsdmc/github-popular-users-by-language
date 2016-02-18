b25c7e7a49a0c86b914fe9e46aae1400e1f31fb2
Dmytro Chepurnyi <dmytro.chepurnyi@gmail.com>
2016-02-18 12:13:29 +0200

    some tests

---
+++ test/actions/usersList.spec.js
@@ -0,0 +1,31 @@
+import expect from 'expect';
+import * as actions from '../../src/redux-base/actions';
+
+describe('app actions', () => {
+  it('selectLanguage should create SELECT_LANGUAGE action', () => {
+    expect(actions.selectLanguage('javascript')).toEqual({
+      type: 'SELECT_LANGUAGE',
+      language: 'javascript'
+    });
+  });
+
+  it('receiveUsers should create RECEIVE_USERS action', () => {
+    const users = [{ login: 'user1' }, { login: 'user2' }];
+    const now = new Date();
+    const action = actions.receiveUsers('python', users);
+
+    const { receivedAt, ...actionWithoutTime } = action;
+
+    expect(actionWithoutTime).toEqual({
+      type: 'RECEIVE_USERS',
+      language: 'python',
+      users
+    });
+
+    expect(new Date(action.receivedAt).getMonth()).toEqual(now.getMonth());
+    expect(new Date(action.receivedAt).getDay()).toEqual(now.getDay());
+    expect(new Date(action.receivedAt).getHours()).toEqual(now.getHours());
+    expect(new Date(action.receivedAt).getMinutes()).toEqual(now.getMinutes());
+
+  });
+});
---
+++ test/reducers/usersByLanguage.spec.js
@@ -0,0 +1,155 @@
+import expect from 'expect';
+import usersByLanguage from '../../src/redux-base/reducers/usersByLanguage';
+import * as actions from '../../src/redux-base/actions';
+
+describe('usersByLanguage reducer', () => {
+  it('should handle initial state', () => {
+    expect(usersByLanguage(undefined, {})).toEqual({});
+  });
+
+  it('should handle REQUEST_USERS action', () => {
+
+    const state = {
+      java: {
+        isFetching: false
+      },
+      python: {
+
+      }
+    };
+
+    expect(
+      usersByLanguage(state, {
+        type: 'REQUEST_USERS',
+        language: 'java'
+      })
+    ).toEqual({
+      java: {
+        isFetching: true
+      },
+      python: {
+
+      }
+    });
+
+  });
+
+
+  it('should handle RECEIVE_USERS action', () => {
+
+    const state = {
+      java: {
+        isFetching: true
+      },
+      python: {
+        items: [
+          { login: 'user1' },
+          { login: 'user2' }
+        ]
+      }
+    };
+
+    let users = [
+      { login: 'user1-java' },
+      { login: 'user2-java' }
+    ];
+    let action = actions.receiveUsers('java', users);
+
+    expect(
+      usersByLanguage(state, action)
+    )
+    .toEqual({
+      ...state,
+      java: {
+        isFetching: false,
+        items: users,
+        lastUpdated: action.receivedAt
+      }
+    });
+
+    users = [
+      { login: 'user1-python' },
+      { login: 'user2-python' }
+    ];
+    action = actions.receiveUsers('python', users);
+
+    expect(
+      usersByLanguage(state, action)
+    )
+    .toEqual({
+      ...state,
+      python: {
+        isFetching: false,
+        items: users,
+        lastUpdated: action.receivedAt
+      }
+    });
+
+  });
+
+  it('should handle RECEIVE_FOLLOWERS_COUNT action', () => {
+
+    const initState = {
+      java: {
+        items: [
+          { login: 'user1-java' },
+          { login: 'user2-java' }
+        ]
+      },
+      python: {
+        items: [
+          { login: 'user1-python' },
+          { login: 'user2-python' }
+        ]
+      }
+    };
+
+    let action = actions.receiveFollowersCount('user2-java', 'java', 12345);
+    let state = usersByLanguage(initState, action);
+
+    expect(
+      state
+    )
+    .toEqual({
+      ...initState,
+      java: {
+        items: [
+          {
+            login: 'user1-java'
+          },
+          {
+            login: 'user2-java',
+            followersCount: 12345
+          }
+        ]
+      }
+    });
+
+    action = actions.receiveFollowersCount('user3-java', 'java', 777);
+    state = usersByLanguage(state, action);
+
+    expect(state).toEqual(state);
+
+    action = actions.receiveFollowersCount('user1-python', 'python', 333);
+    state = usersByLanguage(initState, action);
+
+    expect(
+      state
+    )
+    .toEqual({
+      ...initState,
+      python: {
+        items: [
+          {
+            login: 'user1-python',
+            followersCount: 333
+          },
+          {
+            login: 'user2-python'
+          }
+        ]
+      }
+    });
+
+  });
+});
